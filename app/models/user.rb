14053bd9d74beef170a8cda4de47d422e1bab216
Akito <los24ak@gmail.com>
2022-07-09 14:27:16 +0900

    add:like_core

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  # いいね機能 ここから
+  has_many :likes, dependent: :destroy
+  has_many :like_recipes, through: :likes, source: :recipe
+
+  def like(recipe)
+    like_recipes << recipe
+  end
+
+  def unlike(recipe)
+    like_recipes.destroy(recipe)
+  end
+
+  def like?(recipe)
+    like_recipes.include?(recipe)
+  end
+  #ここまで
 end
