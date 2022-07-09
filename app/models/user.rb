53182c3ed609fdfb2be4d082e374a91eea1469c7
kanada yoshihiro <211050.yk@gmail.com>
2022-07-09 15:26:13 +0900

    Add: 新規登録機能、ログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :email, uniqueness: true, presence: true
+  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: 'recipes#index'
+resources :users
+get 'login', to: 'user_sessions#new'
+post 'login', to: 'user_sessions#create'
+delete 'logout', to: 'user_sessons#destroy'
+resources :recipes do
+	resources :comments
+	collection do 
+		get :likes
+	end
+end
 end
