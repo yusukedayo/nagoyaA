a1aa99ce54acb2e421076fd4cee7010f0d80f9cc
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-07-09 16:32:15 +0900

    レシピ作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,17 +3,28 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @reciepe = Recipe.new
   end
 
-  def show
-  end
+  def show; end
 
   def create
+    @reciepe = Recipe.new(reciepe_params)
+    if @reciepe.save
+      redirect_to root_path, success: 'レシピを作成しました'
+    else
+      flash.now[:success] = '入力内容を見直してください'
+      render :new
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def reciepe_params
+    params.require(:reciepe).permit(:title, :content, :image)
   end
 end
