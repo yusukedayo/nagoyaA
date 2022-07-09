1f798e79e0973a52fc070d87905db6f151ecf700
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-07-09 16:07:25 +0900

    modelとseed記載

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  # validates
+  validates :title, presence: true, lenght: { maximum: 250 }
+  validates :content, presence: true, length: { miximum: 1_000 }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Example User",
+            email: "example@railstutorial.org",
+            password: "foobar",
+            password_confirmation: "foobar")
+
+# 追加のユーザーをまとめて生成する
+99.times do |n|
+  name = "example-#{n+1}"
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(   name: name,
+                  email: email,
+              password: password,
+  password_confirmation: password)
+end
